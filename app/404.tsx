db6837362b99b63fd03800bb8e777f73a95c6abe
Nihat Zaman <nihat.zaman78@erzurum.edu.tr>
2025-05-25 12:49:16 +0300

    fix 404 page

---
+++ app/404.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="text-center mt-24">
+      <h1 className="text-3xl font-bold">
+        404 - Sayfa Bulunamadı
+      </h1>
+      <p className="text-lg mt-4">
+        Aradığınız sayfa mevcut değil.
+      </p>
+    </div>
+  );
+}
